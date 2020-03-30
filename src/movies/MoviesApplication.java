f5a0966fe95b701d60edeb94e702fc8a62f945cd
James Spicer <spicer.r.james@gmail.com>
2020-03-30 14:21:19 -0500

    Square and Rectangle exercises in progress

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,23 +1,23 @@
 package movies;
 
-import util.Input;
-
-public class MoviesApplication {
-
-    final static String[] categories = {"none", "all", "animated", "drama", "horror", "comedy", "scifi", "musical"};
-
-    public static void main(String[] args){
-
-        Input scanner = new Input();
-        Movie[] movieList = MoviesArray.findAll();
-
-        int choice = 1;
-        do {
-            //Instere optins for user here
-
-            try{
-                choice = scanner.getInt("Enter your choice");
-            } catch {NumberFormatException nfe};
-        }
-    }
-}
+//import util.Input;
+//
+//public class MoviesApplication {
+//
+//    final static String[] categories = {"none", "all", "animated", "drama", "horror", "comedy", "scifi", "musical"};
+//
+//    public static void main(String[] args){
+//
+//        Input scanner = new Input();
+//        Movie[] movieList = MoviesArray.findAll();
+//
+//        int choice = 1;
+//        do {
+//            //Instere optins for user here
+//
+//            try{
+//                choice = scanner.getInt("Enter your choice");
+//            } catch {NumberFormatException nfe};
+//        }
+//    }
+//}
--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,29 @@
+/*Inside of your shapes directory, create a class named Rectangle. (DONE)
+
+
+It should have protected properties for both length and width. Rectangle should define a constructor that accepts two
+numbers for length and width, and sets those properties.
+
+Create two methods on the Rectangle class, getArea and getPerimeter that return the respective values. The formulas for
+both follow:
+
+perimeter = 2 x length + 2 x width
+area = length x width*/
+
 package shapes;
 
 public class Rectangle {
+    protected int length = 7;
+    protected int width = 8;
+
+    public int getArea() {
+        int area = length * width;
+        return area;
+    }
+
+    public int getPerimeter() {
+        int perimeter = (2 * length) + (2 * width);
+        return perimeter;
+    }
+
 }
