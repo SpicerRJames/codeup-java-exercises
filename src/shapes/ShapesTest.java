1feda684f13e23738a437c7cca4ff85aa612fb0e
James Spicer <spicer.r.james@gmail.com>
2020-03-31 13:57:49 -0500

    Interface and abstract exercises done

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -13,19 +13,60 @@ Rectangle class with a width of 4 and a length of 5
 
 package shapes;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class ShapesTest {
 
+//    static Measurable myShape = new Measurable(){
+//        @Override
+//        public double getPerimeter() {
+//            System.out.println("Shapes Test 1");
+//            return 5;
+//        }
+//
+//        @Override
+//        public double getArea() {
+//            System.out.println("Shapes Test 1");
+//            return 10;
+//        }
+//    };
+
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(4, 5);
+        Measurable myShape;
+
+
+//        Modify your ShapesTest class, use it to:
+//
+//        declare a variable of the type Measurable named myShape.
+//        Test your code by creating instances of both Square and Rectangle and assigning them to myShape so you can display the shape's area and perimeter.
+
+
+        Rectangle box1 = new Rectangle(3, 9);
+        myShape = box1;
         box1.getArea();
         box1.getPerimeter();
+//
+        System.out.println("\nThe area of the rectangle is: " + myShape.getArea() + "ft");
+//        System.out.println("\nThe perimeter of the rectangle is: " + box1.getPerimeter() + "ft");
+//
+//        Rectangle box2 = new Rectangle(5, 7);
+//        System.out.println("\nThe area of the square is: " + box2.getArea() + "ft");
 
-        System.out.println("\nThe area of the rectangle is: " + box1.getArea() + "ft");
-        System.out.println("\nThe perimeter of the rectangle is: " + box1.getPerimeter() + "ft");
 
-        Rectangle box2 = new Square(5);
-//        box2.sides();
-        System.out.printf("\nThe area of the square is: " + box2.getArea() + "ft");
+
+
+
+
+
+//        Rectangle box1 = new Rectangle(4, 5);
+//        box1.getArea();
+//        box1.getPerimeter();
+//
+//        System.out.println("\nThe area of the rectangle is: " + box1.getArea() + "ft");
+//        System.out.println("\nThe perimeter of the rectangle is: " + box1.getPerimeter() + "ft");
+//
+//        Rectangle box2 = new Rectangle(5, 7);
+//        System.out.println("\nThe area of the square is: " + box2.getArea() + "ft");
 
     }
 }
